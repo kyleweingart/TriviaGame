4a2e9e896365333e59b60521fd56d47d665a6575
Kyle Weingart <kyle.weingart@gmail.com>
2018-03-05 16:30:14 -0500

    Final updates to Liverpool Trivia Game .js and .html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   //  VARIABlES
   // =======================================================================================================
 
@@ -6,7 +6,13 @@ $(document).ready(function () {
 
   var intervalID;
 
-  var questionsArray = ["How many times has Liverpool won the Champions League?", "When was the last time Liverpool won the English Premier League?", "How many times has Liverpool won the English Premier League?", "Who has the most first-team goals for Liverpool?", "When did 'You'll Never Walk Alone' become Liverpool's anthem?"];
+  var questionsArray = [
+    "How many times has Liverpool won the Champions League?",
+    "When was the last time Liverpool won the English Premier League?",
+    "How many times has Liverpool won the English Premier League?",
+    "Who has the most first-team goals for Liverpool?",
+    "When did 'You'll Never Walk Alone' become Liverpool's anthem?"
+  ];
 
   var index = 0;
 
@@ -17,22 +23,25 @@ $(document).ready(function () {
     ["Steven Gerrard", "Ian Rush", "Kenny Dalglish", "Robbie Fowler"],
     ["1949", "1969", "1955", "1962"],
     ["5 Times", "1990", "18 Times", "Ian Rush", "1962"]
-  ]
+  ];
 
-  var gifArray = ["https://media.giphy.com/media/l0Iy294JIZv3s2FEs/giphy.gif",  "assets/images/dalglish.gif", "assets/images/1990.jpeg", "assets/images/ianrush.gif", "assets/images/scarves.jpeg" , "https://media.giphy.com/media/6Wqg7wOTcZnxK/giphy.gif" ];
+  var gifArray = [
+    "https://media.giphy.com/media/l0Iy294JIZv3s2FEs/giphy.gif",
+    "assets/images/dalglish.gif",
+    "assets/images/1990.jpeg",
+    "assets/images/ianrush.gif",
+    "assets/images/scarves.jpeg",
+    "https://media.giphy.com/media/6Wqg7wOTcZnxK/giphy.gif"
+  ];
 
-  var correctAnswers= 0;
-
-
+  var correctAnswers = 0;
 
   // FUNCTIONS
   // ======================================================================================================
 
   // New Question function: empties all DOM elements, appends new question, and calls the runTimer function to start the 15 second timer
 
-
   function newQuestion() {
-
     $(".click-btn").hide();
     number = 16;
     question = questionsArray[index];
@@ -40,254 +49,138 @@ $(document).ready(function () {
 
     runTimer();
 
-    $(".image-display")
-        .empty()
+    $(".image-display").empty();
 
     $(".question-display")
       .empty()
       .append(questionsArray[index]);
 
-
     $("#answer-1")
       .empty()
       .append(answers[index][0]);
-
 
     $("#answer-2")
       .empty()
       .append(answers[index][1]);
 
-
     $("#answer-3")
       .empty()
       .append(answers[index][2]);
 
-
     $("#answer-4")
       .empty()
       .append(answers[index][3]);
-
   }
 
-// runTimer function: 
+  // runTimer function:
   function runTimer() {
     clearInterval(intervalID);
     intervalID = setInterval(decrement, 1000);
   }
 
-
-
+  // threeSeconds function: runs for 5 seconds between questions and displays images during that time
   function threeSeconds() {
     clearInterval(intervalID);
     var questionTimeout = setTimeout(newQuestion, 5000);
-    $(".question-display")
-      .empty()
-      
 
+    $(".question-display").empty();
 
-      $("#answer-1")
+    $("#answer-1").empty();
+
+    $("#answer-2").empty();
+
+    $("#answer-3").empty();
+
+    $("#answer-4").empty();
+
+    if (index < questionsArray.length - 1) {
+      index++;
+    } else {
+      $(".time-display")
         .empty()
+        .append(
+          "That's the final whistle.<br> You put " +
+            correctAnswers / 6 +
+            " of 5 chances into the back of the net."
+        );
 
-
-
-      $("#answer-2")
-        .empty()
-
-
-
-      $("#answer-3")
-        .empty()
-
-
-
-      $("#answer-4")
-        .empty()
-
-
-      if (index < questionsArray.length - 1) {
-        index++;
-
-        console.log(index);
-
-        // working on getting image to show up if question was correct
-        
-
-      } else {
-
-        
-        $(".time-display")
-          .empty()
-          .append("Game Over");
-      
-        clearTimeout(questionTimeout);
-        
-
-      }
-
+      clearTimeout(questionTimeout);
     }
-  
-
+  }
 
   function decrement() {
     number--;
     $(".time-display")
       .empty()
       .append("Time Remaining: " + number);
+
     if (number === 0) {
       // stop();
       $(".time-display")
         .empty()
-        .append("Time's Up");
+        .append("Time's Up- Please wait for the next question.");
       clearInterval(intervalID);
       threeSeconds();
     }
   }
 
-  // function checkAnswer(num) {
-  //   var clickAnswer = answers[num][0]; 
-  //     for (var i = 0; i <= answers[5].length; i++) {
-  //     if (answers[5].indexOf(clickAnswer)=== -1) {
-  //     $(".image-display")
-  //         .empty()
-  //         .append('<img src='  + gifArray[5] +' />');
-  //     }
-  //     else {
-  //       console.log("yep");
-  //       correctAnswers++; 
-  //       console.log(index);
-  //       $(".image-display")
-  //         .empty()
-  //         .append('<img src='  + gifArray[num] +' />');
-  //     }
-  //   }
+  // checkAnswer function- checks to see if user answer is correct - displays proper image depending if answer is right or wrong.
 
-  // }
+  function checkAnswer(num) {
+    var clickAnswer = answers[index][num];
+    for (var i = 0; i <= answers[5].length; i++) {
+      if (answers[5].indexOf(clickAnswer) === -1) {
+        $(".time-display")
+          .empty()
+          .append("Are you a wool?");
+        $(".image-display")
+          .empty()
+          .append("<img src=" + gifArray[5] + " />");
+      } else {
+        correctAnswers++;
+        $(".time-display")
+          .empty()
+          .append("Boss Tha!");
+        $(".image-display")
+          .empty()
+          .append("<img src=" + gifArray[index] + " />");
+      }
+    }
+  }
 
-  
   // MAIN PROCESS
   // ============================================================================================================
 
-  
-  $(".click-btn").on("click", function () {
-      newQuestion();
-    }
+  // click Start Button - kick off the game.
+  $(".click-btn").on("click", function() {
+    newQuestion();
+  });
 
-  )
-  $("#answer-1").on("click", function () {
-      
-    // checkAnswer(index); 
-      
-      var clickAnswer = answers[index][0]; 
-      for (var i = 0; i <= answers[5].length; i++) {
-      if (answers[5].indexOf(clickAnswer)=== -1) {
-      $(".image-display")
-          .empty()
-          .append('<img src='  + gifArray[5] +' />');
-      }
-      else {
-        console.log("yep");
-        correctAnswers++; 
-        console.log(index);
-        $(".image-display")
-          .empty()
-          .append('<img src='  + gifArray[index] +' />');
-      }
-    }
-     
-      threeSeconds();
-     
-        
-       
-      }
+  // click Answer 1
+  $("#answer-1").on("click", function() {
+    checkAnswer(0);
 
-
-      
-    
-
-  )
-  $("#answer-2").on("click", function () {
-
-    var clickAnswer = answers[index][1]; 
-    for (var i = 0; i <= answers[5].length; i++) {
-    if (answers[5].indexOf(clickAnswer)=== -1) {
-    $(".image-display")
-        .empty()
-        .append('<img src='  + gifArray[5] +' />');
-    }
-    else {
-      console.log("yep");
-      correctAnswers++; 
-      console.log(index);
-      $(".image-display")
-        .empty()
-        .append('<img src='  + gifArray[index] +' />');
-    }
-  }
-
-
-
-    
-     
-     
     threeSeconds();
-      
-    }
+  });
 
-  )
-  $("#answer-3").on("click", function () {
+  // click Answer 2
+  $("#answer-2").on("click", function() {
+    checkAnswer(1);
 
-    var clickAnswer = answers[index][2]; 
-    for (var i = 0; i <= answers[5].length; i++) {
-    if (answers[5].indexOf(clickAnswer)=== -1) {
-    $(".image-display")
-        .empty()
-        .append('<img src='  + gifArray[5] +' />');
-    }
-    else {
-      console.log("yep");
-      correctAnswers++; 
-      console.log(index);
-      $(".image-display")
-        .empty()
-        .append('<img src='  + gifArray[index] +' />');
-    }
-  }
-      
-     
-    
-      threeSeconds();
-    }
+    threeSeconds();
+  });
 
-  )
-  $("#answer-4").on("click", function () {
+  // click Answer 3
+  $("#answer-3").on("click", function() {
+    checkAnswer(2);
 
-    var clickAnswer = answers[index][3]; 
-    for (var i = 0; i <= answers[5].length; i++) {
-    if (answers[5].indexOf(clickAnswer)=== -1) {
-    $(".image-display")
-        .empty()
-        .append('<img src='  + gifArray[5] +' />');
-    }
-    else {
-      console.log("yep");
-      correctAnswers++; 
-      console.log(index);
-      $(".image-display")
-        .empty()
-        .append('<img src='  + gifArray[index] +' />');
-    }
-  }
-     
-      
-      
-      threeSeconds();
-    }
+    threeSeconds();
+  });
 
-  )
-  
+  // click Answer 4
+  $("#answer-4").on("click", function() {
+    checkAnswer(3);
 
-
-
-
-
+    threeSeconds();
+  });
 });
